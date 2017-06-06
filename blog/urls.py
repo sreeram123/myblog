6f7987a07c1a6785bc4d586bd0b97c08a0bee747
navisk13 <navisk13@gmail.com>
2017-06-06 15:58:06 +0530

    edit2

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,14 @@
 from django.conf.urls import url
 
-from blog.views import home, post_detail, add_post
+from blog.views import home, post_detail, add_post, edit_post, delete_post
 
 urlpatterns = [
     url(r'^$', home, name="home"),
     url(r'^(?P<id>[0-9]+)/$', post_detail, name="post"),
     url(r'^add/$', add_post, name="add_post"),
+    url(r'^(?P<pk>[0-9]+)/edit', edit_post, name="edit_post"),
+    url(r'^(?P<id>[0-9]+)/delete', delete_post, name="delete_post"),
+
+
+
 ]
--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals
 
 from django.contrib.auth.models import User
+from django.core.exceptions import PermissionDenied
 from django.shortcuts import render, redirect
 
 # Create your views here.
@@ -39,3 +40,32 @@ def add_post(request):
         template_name='blog/add_post.html'
         context = {}
         return render(request, template_name, context)
+
+
+def edit_post(request, pk):
+    post = Post.objects.get(id=int(pk))
+    if request.user.username != post.user.username:
+        raise PermissionDenied
+    if request.method == "GET":
+        template_name = 'blog/post_edit.html'
+        context = {'post' : post}
+        return render(request, template_name, context)
+    else:
+        post.title = request.POST['title']
+        post.content = request.POST['content']
+        if 'img' in request.FILES:
+            post.img = request.FILES["img"]
+        if 'is_published' in request.POST:
+            post.is_published = request.POST['is_published']
+        post.save()
+        return redirect('post', post.id)
+
+
+def delete_post(request, id):
+    if Post.objects.get(id=id) is not None:
+        post = Post.objects.get(id=id)
+        if post.user.username == request.user.username:
+            post.delete()
+            return redirect('home')
+    raise PermissionDenied
+
